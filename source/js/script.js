50e921b4fb5e882d32b071e5984570ecbeba7b18
RamunSalazar <52279521+RamunSalazar@users.noreply.github.com>
2021-01-13 20:48:35 +0300

    Внес изменения по тз

--- source/js/script.js
+++ source/js/script.js
@@ -3,18 +3,22 @@ const navigationToggle = document.querySelector('.page-header__navigation-toggle
 const navigation = document.querySelector('.page-header__navigation-top');
 const addProfileLink = document.querySelector('.add-profile_link');
 const addProfileModal = document.querySelector('.add-profile__modal-wrapper');
-const addProfileCloseButton = addProfileModal.querySelector('.add-profile__modal-button');
+const addProfileRouteWrraper = document.querySelectorAll('.add-plan__route-wrapper');
+const filterCountry = document.querySelector('.filter-country');
+const filterCompanionWrraper = document.querySelectorAll('.filter-companion__wrapper');
 
-for (let i = 0; i < transportButton.length; i++) {
-  transportButton[i].addEventListener('click', function() {
-    if (transportButton[i].classList.contains('direction-form__transport-button--on')) {
-      transportButton[i].classList.remove('direction-form__transport-button--on');
-      transportButton[i].classList.add('direction-form__transport-button--off')
-    } else if (transportButton[i].classList.contains('direction-form__transport-button--off')) {
-      transportButton[i].classList.remove('direction-form__transport-button--off');
-      transportButton[i].classList.add('direction-form__transport-button--on')
-    }
-  });
+if (transportButton) {
+  for (let i = 0; i < transportButton.length; i++) {
+    transportButton[i].addEventListener('click', function() {
+      if (transportButton[i].classList.contains('direction-form__transport-button--on')) {
+        transportButton[i].classList.remove('direction-form__transport-button--on');
+        transportButton[i].classList.add('direction-form__transport-button--off')
+      } else if (transportButton[i].classList.contains('direction-form__transport-button--off')) {
+        transportButton[i].classList.remove('direction-form__transport-button--off');
+        transportButton[i].classList.add('direction-form__transport-button--on')
+      }
+    });
+  }
 }
 
 navigationToggle.addEventListener('click', function() {
@@ -25,11 +29,56 @@ navigationToggle.addEventListener('click', function() {
   }
 });
 
-addProfileLink.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  addProfileModal.classList.add('add-profile__modal-wrapper--open');
-});
+if (addProfileLink) {
+  const addProfileCloseButton = addProfileModal.querySelector('.add-profile__modal-button');
 
-addProfileCloseButton.addEventListener('click', function() {
-  addProfileModal.classList.remove('add-profile__modal-wrapper--open');
-});
+  addProfileLink.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    addProfileModal.classList.add('add-profile__modal-wrapper--open');
+  });
+
+  addProfileCloseButton.addEventListener('click', function() {
+    addProfileModal.classList.remove('add-profile__modal-wrapper--open');
+  });
+}
+
+if (addProfileRouteWrraper.length) {
+  const addProfileRouteImage = addProfileRouteWrraper[2].querySelectorAll('.add-plan__route-image');
+
+  addProfileRouteImage[0].addEventListener('click', function() {
+    addProfileRouteWrraper[2].classList.add('add-plan__route-wrapper--open');
+  });
+
+  addProfileRouteImage[1].addEventListener('click', function() {
+    addProfileRouteWrraper[2].classList.remove('add-plan__route-wrapper--open');
+  });
+}
+
+if (filterCountry) {
+  const filterCountryToggle = filterCountry.querySelector('.filter-country__toggle');
+  const filterCountryButtonClose = filterCountry.querySelector('.filter-country__button');
+  filterCountryToggle.addEventListener('click', function() {
+    if (filterCountry.classList.contains('filter-country--open')) {
+      filterCountry.classList.remove('filter-country--open');
+    } else {
+      filterCountry.classList.add('filter-country--open');
+    }
+  });
+
+  filterCountryButtonClose.addEventListener('click', function() {
+    filterCountry.classList.remove('filter-country--open');
+  });
+}
+
+if (filterCompanionWrraper.length) {
+  const filterCompanionName = document.querySelectorAll('.filter-companion__name');
+  for (let i = 0; i < filterCompanionName.length; i++) {
+    filterCompanionName[i].addEventListener('click', function() {
+      if (filterCompanionWrraper[i].classList.contains('filter-companion__wrapper--open')) {
+        filterCompanionWrraper[i].classList.remove('filter-companion__wrapper--open');
+      } else {
+        filterCompanionWrraper[i].classList.add('filter-companion__wrapper--open');
+      }
+    });
+  }
+}
